e9283dc803a031dbb4ee29521226111b97ac750a
victor <duprezvictor4@gmail.com>
2023-12-13 11:10:40 +0100

    commit mercredi

--- src/App.js
+++ src/App.js
@@ -9,20 +9,12 @@ const loading = (
 )
 
 const DefaultLayout = React.lazy(() => import('./layout/DefaultLayout'))
-const Login = React.lazy(() => import('./views/pages/login/Login'))
-const Register = React.lazy(() => import('./views/pages/register/Register'))
-const Page404 = React.lazy(() => import('./views/pages/page404/Page404'))
-const Page500 = React.lazy(() => import('./views/pages/page500/Page500'))
 
 const App = () => {
   return (
     <HashRouter>
       <Suspense fallback={loading}>
         <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/404" element={<Page404 />} />
-          <Route path="/500" element={<Page500 />} />
           <Route path="*" element={<DefaultLayout />} />
         </Routes>
       </Suspense>
